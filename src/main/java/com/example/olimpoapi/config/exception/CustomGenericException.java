99d0bfe6e1a87847ea1a56d7cff16ae9d5eec0d4
RaphaCosil <Raphael.Cosil@gmail.com>
2024-09-24 11:45:51 -0300

    ADD - Melhorando estrutura de pacotes

--- src/main/java/com/example/olimpoapi/config/exception/CustomGenericException.java
+++ src/main/java/com/example/olimpoapi/config/exception/CustomGenericException.java
@@ -1,4 +1,4 @@
-package com.example.olimpoapi.exception;
+package com.example.olimpoapi.config.exception;
 
 public class CustomGenericException extends RuntimeException {
     public CustomGenericException(String message) {
