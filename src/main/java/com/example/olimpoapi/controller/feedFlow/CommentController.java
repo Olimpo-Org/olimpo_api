9c27f337b436ee49f4a8d7b475ea616bd0250c65
RaphaCosil <Raphael.Cosil@gmail.com>
2024-09-25 12:01:01 -0300

    ADD/FIX - criando ID em publicação e fazendo o fluxo de comentário funcionar

--- src/main/java/com/example/olimpoapi/controller/feedFlow/CommentController.java
+++ src/main/java/com/example/olimpoapi/controller/feedFlow/CommentController.java
@@ -3,6 +3,8 @@ package com.example.olimpoapi.controller.feedFlow;
 import com.example.olimpoapi.config.exception.ExceptionThrower;
 import com.example.olimpoapi.model.mongo.Comment;
 import com.example.olimpoapi.service.feedFlow.CommentService;
+import com.example.olimpoapi.utils.GsonUtils;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -15,17 +17,18 @@ import java.util.List;
 @RequestMapping("/v1/comment")
 public class CommentController {
     private final CommentService commentService;
-    private Validator validator;
+    private final GsonUtils gsonUtils;
     @Autowired
     public CommentController(CommentService commentService) {
         this.commentService = commentService;
+        this.gsonUtils = new GsonUtils();
     }
     @GetMapping("/get/publication/{publicationId}")
     public ResponseEntity<String> getAllOfPublication(
             @PathVariable("publicationId") String publicationId
     ) {
         List<Comment> comments = commentService.getAllOfPublication(publicationId);
-        return ResponseEntity.ok().body(comments.toString());
+        return ResponseEntity.ok().body(gsonUtils.toJson(comments));
     }
     @PostMapping("/create")
     public ResponseEntity<String> create(@RequestBody Comment comment, BindingResult result) {
@@ -33,7 +36,7 @@ public class CommentController {
             ExceptionThrower.throwBadRequestException(result.getAllErrors().get(0).getDefaultMessage());
         }
         Comment createdComment = commentService.create(comment);
-        return ResponseEntity.ok().body(createdComment.toString());
+        return ResponseEntity.ok().body(gsonUtils.toJson(createdComment));
     }
 
 }
